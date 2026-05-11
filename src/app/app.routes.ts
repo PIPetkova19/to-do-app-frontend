56a2a3b36ae663d7b081fec5d88cc88af4c33e6d
PIPetkova19 <PIPetkova19@codingburgas.bg>
2026-05-11 17:50:43 +0300

    Add view for users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/components/user';
+import { HomeComponent } from './home/component/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UserComponent }
+];
---
+++ src/app/user/components/user.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { UserService } from '../services/user';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user',
+    standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user.html'
+})
+export class UserComponent implements OnInit {
+
+  users: any[] = [];
+
+  constructor(private service: UserService, private cdr: ChangeDetectorRef) {}
+
+ngOnInit(): void {
+    this.service.getUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.log('ERROR:', err);
+      }
+    });
+  }
+}
